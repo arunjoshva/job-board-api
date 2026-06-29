6fe9d716ed513727e91ea680ec5a2e1df57f1dda
arunjoshva <arjose1993@gmail.com>
2026-06-29 13:09:52 +0530

    root path response changed

--- src/app.js
+++ src/app.js
@@ -14,7 +14,10 @@ app.use("/api/applications", applicationRoutes); //Mounting Application Routes i
 
 app.get("/", (req,res) => {
     res.status(200).json(
-        {   
+        {  
+            project: "Job Board REST API",
+            status: "Running",
+            version: "1.0.0", 
             sample_endpoint: "/api/jobs",
             message:"For other endpoints, view on github"}
     );
